c189ffaf06e0f957e5f6be81321fe85dc38f3e28
Mate9000 <1213286@student.roc-nijmegen.nl>
2024-11-27 12:15:01 +0100

    arrays!

---
+++ src/masterMind2/Testzone.java
@@ -0,0 +1,20 @@
+package masterMind2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Testzone {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random random = new Random();
+
+		int[] numbers = new int[4];
+		for (int e = 0; e < numbers.length; e++) {
+			numbers[e] = (int) random.nextInt(6);
+	        String secretCode = "" + numbers[e];
+	        System.out.print(secretCode);
+		}
+	}
+
+}
