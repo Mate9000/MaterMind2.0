a32e5175ad535c5612ded16f8e8e0929e0e840bc
Mate9000 <1213286@student.roc-nijmegen.nl>
2025-01-06 15:04:06 +0100

    meer stuff

---
+++ src/conditionalex/Calculator_week7.java
@@ -0,0 +1,12 @@
+package conditionalex;
+
+public class Calculator_week7 {
+    public int add(int x, int y){
+        return x + y;
+    }
+    
+    // This method could throw an ArithmeticException
+    public double divide(int x, int y) {
+        return x / y;
+    }
+}
---
+++ src/conditionalex/ExceptionEx1.java
@@ -0,0 +1,18 @@
+package conditionalex;
+
+public class ExceptionEx1 {
+
+    public static void main(String args[]) {
+
+        int[] vals = {3, 2, 6, 1,};
+        
+        int total = 0;
+
+        for (int i = 0; i < vals.length; i++) {
+            int index = vals[i];
+            total += vals[i];
+        }
+         System.out.println(total);
+    }
+
+}
