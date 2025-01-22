ec6f931a623260d4f38e9062df79a17a00cd18c8
Mate9000 <1213286@student.roc-nijmegen.nl>
2025-01-22 14:24:12 +0100

    comments

--- src/masterMind2/Matecodebreaker02.java
+++ src/masterMind2/Matecodebreaker02.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class MateCodeBreaker02 {
 	public static void main(String[] args) {
 		MasterMath gen = new MasterMath();
+		//this integer is the max attempts you're giving
 		int maxAttempts = 10;
+		//scanner is here so u can actually place guesses
 		Scanner sc = new Scanner(System.in);
+		//the length of the code
 		int[] numbers = new int[4];
+		//this here makes random code
 		String secretCode = gen.generator(numbers);
-		System.out.println("only 4 digit code allowed otherwise game crashes");
+		
+		System.out.println("Make You're guess from 0-5 have fun :3");
 		for (int i = 1; i <= maxAttempts; i++) {
 			System.out.println("Attempt " + i + ": Enter your 4-digit guess from 0 to 5:");
 			int[] guess = gen.guessInput();
